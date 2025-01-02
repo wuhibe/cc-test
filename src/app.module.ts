62b9160adec987cf4a2efa50c11dff1af6abb096
wuhibe <wuhibetamire710@gmail.com>
2025-01-02 09:38:35 +0300

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CommentReplyModule } from './comment-reply/comment-reply.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CommentReplyModule],
+  imports: [PrismaModule, CommentReplyModule],
   controllers: [AppController],
 })
 export class AppModule {}
